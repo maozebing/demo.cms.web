48846d931e3d753046a413c5d40afbdae5d4ff58
maozebing <mzb0312@163.com>
2019-03-01 13:31:01 +0800

    1

---
+++ src/pages/Running.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Running"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'home',
+    component: resolve => require(['../App.vue'], resolve),
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        redirect: {name: 'running'},
+        component: resolve => require(['../pages/Home.vue'], resolve),
+        //meta: { requiresAuth: true ,keepAlive:false},
+        children: [
+          {
+            path: 'running',
+            name: 'running',
+            component: resolve => require(['../pages/Running.vue'], resolve),
+            //meta: { requiresAuth: true }
+          }
+        ]
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  /*  mode: 'history',*/
+  routes
+});
+
+export default router;
+
+
+
